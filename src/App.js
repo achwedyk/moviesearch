f85dfa12a02bf78307950a8c681a0abcfa31812e
Adam Chwedyk <adam.chwedyk@toptal.com>
2020-03-01 23:11:32 +0000

    Fix duplicated key in the list

--- src/App.js
+++ src/App.js
@@ -24,18 +24,22 @@ const callMovieDB = (route, params = {}) => {
     .then(response => response.json());
 }
 
-// returns array of strings - movie titles
+// returns array of objects with the following structure:
+//   id: movie id
+//   title: movie title
 const fetchMovieSuggestions = (query, limit = 5) => {
   return callMovieDB('/search/movie', {query, include_adult: false})
     .then(movies => {
       let list = movies.results;
       let suggestions = [];
       for (let i = 0; i < Math.min(limit, list.length); i++) {
-        suggestions.push(list[i].title);
+        const { id, title } = list[i];
+        suggestions.push({ id, title });
       }
       return suggestions;
     })
     .catch(err => {
+      console.error(err);
       return [];
     });
 };
@@ -59,7 +63,9 @@ class App extends Component {
     e.preventDefault();
 
     fetchMovieSuggestions(this.state.query)
-      .then(suggestions => this.setState({ suggestions }));
+      .then(suggestions => {
+        this.setState({ suggestions });
+      });
   }
 
   render() {
@@ -73,14 +79,14 @@ class App extends Component {
           </h1>
         </header>
         <form className="search" onSubmit={this.onSubmit}>
-          <input className="search__input" autoFocus={true} 
+          <input className="search__input" autoFocus={true}
             placeholder="Type to search movie title..." onChange={this.onQueryChange}
             value={query} list="suggestions" id="query" name="query"/>
           <datalist id="suggestions">
-            {suggestions.map(val => 
-              <option key={val} value={val}/>
+            {suggestions.map(({ id, title }) =>
+              <option key={id} value={title}/>
             )}
-          </datalist> 
+          </datalist>
         </form>
       </div>
     );
